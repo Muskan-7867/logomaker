d3088419b1a49d0e368d66629d259e90538803a4
Muskan-7867 <muskanloach984@gmail.com>
2026-02-16 16:53:59 +0530

    add props

--- components/ToolButton.tsx
+++ components/ToolButton.tsx
@@ -5,19 +5,22 @@ export const ToolButton = ({
   onClick,
 }: {
   label: string;
-  icon: string;
+  icon?: string;
   active?: boolean;
   onClick?: () => void;
 }) => (
   <button
     onClick={onClick}
-    className={`w-full flex items-center gap-3 px-3 py-2.5 rounded transition-colors ${
+    className={`w-full flex items-center  px-3 py-1.5 justify-center rounded transition-colors ${
       active
         ? "bg-zinc-800 text-white"
-        : "text-gray-400 hover:bg-zinc-800/50 hover:text-white"
+        : "text-gray-400 hover:bg-zinc-800/50 hover:text-white border border-white/10"
     }`}
   >
     <span className="text-lg">{icon}</span>
-    <span className="text-sm">{label}</span>
+
+    <span className="text-sm ">{label}</span>
   </button>
 );
+
+
