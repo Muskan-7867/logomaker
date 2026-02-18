128906b6f60743986f8311570f65a692f585c95d
Muskan-7867 <muskanloach984@gmail.com>
2026-02-18 12:49:56 +0530

    Gradient bg file and array of gradients bg file

--- components/Gradinets.tsx
+++ components/Gradinets.tsx
@@ -1,3 +1,5 @@
+import { bgGradients } from "./gradients";
+
 type IconSettings = {
   size: number;
   rotate: number;
@@ -20,7 +22,7 @@ function Gradinets({
   updateIconSetting,
 }: Props) {
   return (
-    <div className="grid grid-cols-3 gap-2 pb-4">
+    <div className="grid grid-cols-4 gap-4 pb-4">
       <button
         className={`px-3 py-2 rounded text-xs transition-colors border ${
           iconSettings.backgroundType === "solid"
@@ -31,91 +33,27 @@ function Gradinets({
       >
         Solid
       </button>
-      <button
-        className={`w-24 h-24 rounded-md transition-all border-2 overflow-hidden flex items-center justify-center ${
-          iconSettings.backgroundType === "gradient" &&
-          iconSettings.selectedGradient === "gradient"
-            ? "border-white ring-2 ring-white/20"
-            : "border-white/5 hover:border-white/20"
-        }`}
-        onClick={() => {
-          onIconSettingsChange({
-            ...iconSettings,
-            backgroundType: "gradient",
-            selectedGradient: "gradient",
-          });
-        }}
-      >
-        <div className="w-full h-full gradient" />
-      </button>
-      <button
-        className={`w-24 h-24 rounded-md transition-all border-2 overflow-hidden flex items-center justify-center ${
-          iconSettings.backgroundType === "gradient" &&
-          iconSettings.selectedGradient === "gradient2"
-            ? "border-white ring-2 ring-white/20"
-            : "border-white/5 hover:border-white/20"
-        }`}
-        onClick={() => {
-          onIconSettingsChange({
-            ...iconSettings,
-            backgroundType: "gradient",
-            selectedGradient: "gradient2",
-          });
-        }}
-      >
-        <div className="w-full h-full gradient2" />
-      </button>
-      <button
-        className={`w-24 h-24 rounded-md transition-all border-2 overflow-hidden flex items-center justify-center ${
-          iconSettings.backgroundType === "gradient" &&
-          iconSettings.selectedGradient === "gradient3"
-            ? "border-white ring-2 ring-white/20"
-            : "border-white/5 hover:border-white/20"
-        }`}
-        onClick={() => {
-          onIconSettingsChange({
-            ...iconSettings,
-            backgroundType: "gradient",
-            selectedGradient: "gradient3",
-          });
-        }}
-      >
-        <div className="w-full h-full gradient3" />
-      </button>
-      <button
-        className={`w-24 h-24 rounded-md transition-all border-2 overflow-hidden flex items-center justify-center ${
-          iconSettings.backgroundType === "gradient" &&
-          iconSettings.selectedGradient === "gradient4"
-            ? "border-white ring-2 ring-white/20"
-            : "border-white/5 hover:border-white/20"
-        }`}
-        onClick={() => {
-          onIconSettingsChange({
-            ...iconSettings,
-            backgroundType: "gradient",
-            selectedGradient: "gradient4",
-          });
-        }}
-      >
-        <div className="w-full h-full gradient4" />
-      </button>
-      <button
-        className={`w-24 h-24 rounded-md transition-all border-2 overflow-hidden flex items-center justify-center ${
-          iconSettings.backgroundType === "gradient" &&
-          iconSettings.selectedGradient === "gradient5"
-            ? "border-white ring-2 ring-white/20"
-            : "border-white/5 hover:border-white/20"
-        }`}
-        onClick={() => {
-          onIconSettingsChange({
-            ...iconSettings,
-            backgroundType: "gradient",
-            selectedGradient: "gradient5",
-          });
-        }}
-      >
-        <div className="w-full h-full gradient5" />
-      </button>
+
+      {bgGradients.map((g, index) => (
+        <button
+          key={index}
+          className={`w-16 h-16 rounded-md transition-all border-2 overflow-hidden flex items-center justify-center ${
+            iconSettings.backgroundType === "gradient" &&
+            iconSettings.selectedGradient === g.className
+              ? "border-white ring-2 ring-white/20"
+              : "border-white/5 hover:border-white/20"
+          }`}
+          onClick={() => {
+            onIconSettingsChange({
+              ...iconSettings,
+              backgroundType: "gradient",
+              selectedGradient: g.className,
+            });
+          }}
+        >
+          <div className="w-full h-full" style={{ background: g.background }} />
+        </button>
+      ))}
     </div>
   );
 }
--- components/gradients.tsx
+++ components/gradients.tsx
@@ -83,4 +83,76 @@ export const bgGradients = [
       #af887e 100%
     )`,
   },
+
+  {
+    name: "gradient6",
+    className: "gradient6",
+    /* https://grabient.com/HQNgTGA0wMwgnNEBGADEkNozAdmrsosACzJakkCs01y0VekMpq6oJJ0yYXywbGvzbphIEEA?angle=225 */
+
+    background: `linear-gradient(225deg, 
+    #ff78ff 0.000%, #ff9eff 16.667%, #ffbdff 33.333%, #ffcfeb 50.000%, #ffcf94 66.667%, #e8be47 83.333%, #9f9f13 100.000%)`,
+  },
+
+  {
+    name: "gradient7",
+    className: "gradient7",
+    /* https://grabient.com/HQVgjAnANKAMswCwA4wwEwDZMZA4W6MA7MQMwnLEkjRnDFhFgFgWauIbL6bHTBUIKLCA?angle=225 */
+
+    background: `linear-gradient(225deg, #c1d1cc 0.000%, #dfb4af 16.667%, #f4a09a 33.333%, #f99d95 50.000%, #ebaba2 66.667%, #d0c6bc 83.333%, #b3e1d9 100.000%)`,
+  },
+  {
+    name: "gradient8",
+    className: "gradient8",
+    /* https://grabient.com/HQVgjAnANKAMswCwA4wwEwDZMZA4W6MA7MQMwnLEkjRnDFhFgFgWauIbL6bHTBUIKLCA?angle=225 */
+/* https://grabient.com/HQVg7AnANKAMCMMQGYAcSAsYZgGyxlwCZFgTthldkZ4JsCjhYIMoNh5VpPwp5msAgOQZSsIhCA?angle=225 */
+
+    background: `linear-gradient(225deg, #ffffd1 0.000%, #ffae90 25.000%, #fb2654 50.000%, #be0028 75.000%, #801214 100.000%)`,
+  },
+
+  {
+    name: "gradient9",
+    className: "gradient9",
+ 
+
+background: `linear-gradient(225deg, #2e3c45 0.000%, #2d454e 3.030%, #2f5057 6.061%, #355d61 9.091%, #3c6b6b 12.121%, #477974 15.152%, #54877d 18.182%, #629585 21.212%, #73a28c 24.242%, #84ad91 27.273%, #96b796 30.303%, #a7bf99 33.333%, #b8c59a 36.364%, #c8c89a 39.394%, #d7c999 42.424%, #e3c796 45.455%, #edc391 48.485%, #f4bc8b 51.515%, #f9b484 54.545%, #faa97c 57.576%, #f99d74 60.606%, #f4906a 63.636%, #ed8261 66.667%, #e37457 69.697%, #d6664e 72.727%, #c85845 75.758%, #b74c3d 78.788%, #a64135 81.818%, #95392f 84.848%, #83322a 87.879%, #722d27 90.909%, #622b25 93.939%, #532c25 96.970%, #462f26 100.000%)`,
+  },
+  {
+    name: "gradient10",
+    className: "gradient10",
+
+
+background: `linear-gradient(225deg, #dd8135 0.000%, #ffb258 16.667%, #ffde7d 33.333%, #ffffa2 50.000%, #ffffc5 66.667%, #dcffe4 83.333%, #a4fffd 100.000%)`,
+  },
+  {
+    name: "gradient11",
+    className: "gradient11",
+
+
+background: `linear-gradient(225deg, #b4e1e9 0.000%, #c6f3e7 16.667%, #d5f2e0 33.333%, #e0dfd5 50.000%, #e7bcc6 66.667%, #e890b5 83.333%, #e561a1 100.000%)`,
+  },
+  {
+    name: "gradient12",
+    className: "gradient12",
+ 
+
+background: `linear-gradient(225deg, #70deff 0.000%, #7df4ff 20.000%, #92ffff 40.000%, #aefeff 60.000%, #d2f3ff 80.000%, #fdddff 100.000%)`,
+  },
+
+  {
+    name: "gradient13",
+    className: "gradient13",
+/* https://grabient.com/HQFgbAHANAjMFhlUYzIEwHYCst4AYBOZAZjH2REyWBmxMogeG02lCeWwgiA?angle=225 */
+
+background: `linear-gradient(225deg, #391029 0.000%, #560f52 20.000%, #7f1f89 40.000%, #a43fb9 60.000%, #bb6fd1 80.000%, #baacca 100.000%)`,
+  },
+
+  {
+    name: "gradient14",
+    className: "gradient14",
+
+
+/* https://grabient.com/HQFgrGA0C0bA7AJgJyWGZA2NJkAZJN0wQ0BGRbYTTM8vAZkjOAZETQYA4PhkzSoTPCA?angle=225 */
+
+background: `linear-gradient(225deg, #180079 0.000%, #00388c 16.667%, #107bb4 33.333%, #57abad 50.000%, #abc883 66.667%, #e6d27c 83.333%, #ecc9a4 100.000%)`,
+  }
 ];
